9aadd725452a74463718f3d6fa41fbf4a056b5ec
fazyfirozifar <fazyfirozifar@gmail.com>
2025-10-04 19:03:41 +0330

    feature(src/component/GoogleMapComponent): adding markers

--- src/components/map/GoogleMapComponent.vue
+++ src/components/map/GoogleMapComponent.vue
@@ -1,5 +1,12 @@
 <template>
-  <div ref="mapElement" style="width: 375px; max-width: 100%; height: 400px" />
+  <div
+    ref="mapElement"
+    :style="{
+      width: `${width}px`,
+      height: `${height}px`,
+      maxWidth: '100%',
+    }"
+  />
 </template>
 
 <script setup>
@@ -10,6 +17,36 @@ const GOOGLE_MAP_API_KEY = 'AIzaSyC72ke4z-JR-oJFKuGcb-cUYw3Yhp5_Vv0'
 const mapElement = ref(null)
 let map = null
 
+defineOptions({
+  name: 'GoogleMapComponent',
+})
+
+const props = defineProps({
+  markers: {
+    type: Array,
+    default: () => [],
+  },
+  zoom: {
+    type: Number,
+    default: 12,
+  },
+  center: {
+    type: Object,
+    default: () => ({
+      lat: 36.2972,
+      lng: 59.6067,
+    }),
+  },
+  width: {
+    type: Number,
+    default: 375,
+  },
+  height: {
+    type: Number,
+    default: 400,
+  },
+})
+
 onMounted(async () => {
   setOptions({
     key: GOOGLE_MAP_API_KEY,
@@ -17,8 +54,24 @@ onMounted(async () => {
 
   const { Map } = await importLibrary('maps')
   map = new Map(mapElement.value, {
-    center: { lat: 36.2972, lng: 59.6067 },
-    zoom: 12,
+    center: props.center,
+    zoom: props.zoom,
   })
+
+  if (props.markers.length > 0)
+    props.markers.forEach(marker => {
+      new google.maps.Marker({
+        map,
+        position: { lat: marker.lat, lng: marker.lng },
+        icon: {
+          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+          fillColor: 'blue',
+          fillOpacity: 0.6,
+          strokeWeight: 0,
+          rotation: marker.heading,
+          scale: 10,
+        },
+      })
+    })
 })
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,14 +2,22 @@
   <q-page class="flex flex-center">
     <div class="column items-center q-gutter-md">
       <h4>Google Map</h4>
-      <google-map-component />
+      <google-map-component :markers="markers" />
     </div>
   </q-page>
 </template>
 
 <script setup>
 import GoogleMapComponent from 'src/components/map/GoogleMapComponent.vue'
+
 defineOptions({
   name: 'IndexPage',
 })
+
+const markers = [
+  { lat: 36.2972, lng: 59.6067, heading: 0 },
+  { lat: 36.3035, lng: 59.582, heading: 90 },
+  { lat: 36.286, lng: 59.6157, heading: 180 },
+  { lat: 36.31, lng: 59.63, heading: 270 },
+]
 </script>
