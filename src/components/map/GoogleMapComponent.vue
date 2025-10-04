71f7e6d10cf23146b7dc53921c73a9e8a9c85d17
fazyfirozifar <fazyfirozifar@gmail.com>
2025-10-04 18:24:54 +0330

    -feature(src/components): added google map component, initail config -added google map package

---
+++ src/components/map/GoogleMapComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="mapElement" style="width: 375px; max-width: 100%; height: 400px" />
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { setOptions, importLibrary } from '@googlemaps/js-api-loader'
+
+const GOOGLE_MAP_API_KEY = 'AIzaSyC72ke4z-JR-oJFKuGcb-cUYw3Yhp5_Vv0'
+const mapElement = ref(null)
+let map = null
+
+onMounted(async () => {
+  setOptions({
+    key: GOOGLE_MAP_API_KEY,
+  })
+
+  const { Map } = await importLibrary('maps')
+  map = new Map(mapElement.value, {
+    center: { lat: 36.2972, lng: 59.6067 },
+    zoom: 12,
+  })
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,15 @@
 <template>
-  <q-page class="flex flex-center"> Index </q-page>
+  <q-page class="flex flex-center">
+    <div class="column items-center q-gutter-md">
+      <h4>Google Map</h4>
+      <google-map-component />
+    </div>
+  </q-page>
 </template>
 
 <script setup>
-//
+import GoogleMapComponent from 'src/components/map/GoogleMapComponent.vue'
+defineOptions({
+  name: 'IndexPage',
+})
 </script>
